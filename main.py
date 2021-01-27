a95a8365d074e26446f79f9e8b2f17dfc484538a
zw007981 <zw007981@outlook.com>
2021-01-27 17:54:54 +0800

    完善代码注释。

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ if __name__ == "__main__":
     racks_manager = RacksManager(10, 8)
     # 设定这些移动货架当前处于哪一个巷道。
     racks_manager.setRacksPositions([1, 2, 3, 4, 5, 6, 7, 8])
-    # 此处的request_alley使机器人申请打开通过的巷道。
+    # 此处的request_alley是机器人申请打开通过的巷道。
     A_star = AStarAlgorithm(racks_manager.getRacksPositions(), racks_manager.capacity,
                             request_alley=4)
     solution = A_star.findSolution()
+    # 打印结果。
     if (len(solution) == 0):
         print("Unable to find solution.")
     else:
